a74b9204ce2d2c333c190e200bec9dd6e604cdbb
Ahmed khaled <ahmedkh8907@gmail.com>
2024-07-04 08:56:12 +0300

    adding splash page

---
+++ lib/core/utils/constants/assets.dart
@@ -0,0 +1,44 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAuthBg
+  /// assets/images/auth_bg.png
+  static const String imagesAuthBg = "assets/images/auth_bg.png";
+
+  /// Assets for imagesChooseModeBg
+  /// assets/images/choose_mode_bg.png
+  static const String imagesChooseModeBg = "assets/images/choose_mode_bg.png";
+
+  /// Assets for imagesHomeArtist
+  /// assets/images/home_artist.png
+  static const String imagesHomeArtist = "assets/images/home_artist.png";
+
+  /// Assets for imagesIntroBg
+  /// assets/images/intro_bg.png
+  static const String imagesIntroBg = "assets/images/intro_bg.png";
+
+  /// Assets for vectorsBottomPattern
+  /// assets/vectors/bottom_pattern.svg
+  static const String vectorsBottomPattern = "assets/vectors/bottom_pattern.svg";
+
+  /// Assets for vectorsHomeTopCard
+  /// assets/vectors/home_top_card.svg
+  static const String vectorsHomeTopCard = "assets/vectors/home_top_card.svg";
+
+  /// Assets for vectorsMoon
+  /// assets/vectors/moon.svg
+  static const String vectorsMoon = "assets/vectors/moon.svg";
+
+  /// Assets for vectorsSpotifyLogo
+  /// assets/vectors/spotify_logo.svg
+  static const String vectorsSpotifyLogo = "assets/vectors/spotify_logo.svg";
+
+  /// Assets for vectorsSun
+  /// assets/vectors/sun.svg
+  static const String vectorsSun = "assets/vectors/sun.svg";
+
+  /// Assets for vectorsTopPattern
+  /// assets/vectors/top_pattern.svg
+  static const String vectorsTopPattern = "assets/vectors/top_pattern.svg";
+}
+
---
+++ lib/features/presentation/pages/splash/splash.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/utils/constants/assets.dart';
+
+import '../../../../core/utils/resources/values_manager.dart';
+import '../get_started/get_started.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          Assets.vectorsSpotifyLogo,
+        ),
+      ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(
+      const Duration(seconds: AppDuration.d2),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const GetStartedPage(),
+        ),
+      ),
+    );
+  }
+}
